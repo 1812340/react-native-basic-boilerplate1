995f29fd8f17877d5d5759ad5117457048b0d5ed
waqas ghori <waqasghori@waqass-MacBook-Pro.local>
2025-09-15 19:11:18 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "./globals.css"
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+       <Text className="text-blue-700">
+        wellcome here, This is the basics of react native in which i just integrated the tailwind css and typescript. its like a boilerpate 
+      </Text>
+    </View>
+  );
+}
